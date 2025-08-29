810839dc889f23862b2aa869d00120a5232f37b9
Christian Grévisse <christian.grevisse@uni.lu>
2025-08-29 12:06:36 +0200

    Version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_tagmap';
-$plugin->release = '0.1.1';
-$plugin->version = 2025081901;
+$plugin->release = '0.1.2';
+$plugin->version = 2025082900;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
